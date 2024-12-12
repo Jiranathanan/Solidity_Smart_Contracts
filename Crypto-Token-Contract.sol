9a01298731092d2105de7bb7a29f750133a1e5c7
Jiranathanan <jiranathanan@gmail.com>
2024-12-12 23:01:08 +0700

    Create CryptoToken smart contract

---
+++ Crypto-Token-Contract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+// the uptick arrow ^ defiens a range compile version afther the non zero digit upwards to the next increment of the the non zero number
+
+contract CryptoToken {
+    // uint private tokens = 4000000;
+
+    uint private totalSupply = 7;
+    address public sender;
+    
+    address public minter;
+}
