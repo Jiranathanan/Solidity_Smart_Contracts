bf9341994246d974ac5e0aa84e24fd8b2142a20a
Jiranathanan <jiranathanan@gmail.com>
2024-12-13 04:22:58 +0700

    Create send function and emit an event

--- Crypto-Token-Contract.sol
+++ Crypto-Token-Contract.sol
@@ -8,21 +8,35 @@ contract CryptoToken {
     // mapping(key => value) public mapName
     mapping(address => uint) public balances;
 
+    address public minter;
+
     // Events allow clients to react to specific contract
     // Types of events: Sent Transfer Allow
     // Events take 2 steps: 1. Setting up, 2. Emitting the event
     event Sent(address from, address to, uint amount);
-    
-    // uint private tokens = 4000000;
-
-    uint private totalSupply = 7;
-    address public sender;
-    
-    address private minter;
 
     // a constructor is a special type of function that gets called immediately upon deployment
     constructor() {
         minter = msg.sender;
     } 
+    
+    // build a minting function to mint tokens
+    function mint(address receiver, uint amount) public {
+        // make sure that only owner can mint tokens by using requirement method
+        require(msg.sender == minter);
+        // sets the amount to a specific address
+        balances[receiver] += amount;
+
+    }
+
+    function send(address receiver, uint amount) public {
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+
+    // address public sender;
+
+
 
 }
