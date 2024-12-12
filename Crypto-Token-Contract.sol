e4b685c4a3815fb7d7cdceefcd661b63b223b01c
Jiranathanan <jiranathanan@gmail.com>
2024-12-13 02:55:58 +0700

    Create mapping

--- Crypto-Token-Contract.sol
+++ Crypto-Token-Contract.sol
@@ -3,10 +3,21 @@ pragma solidity ^0.8.4;
 // the uptick arrow ^ defiens a range compile version afther the non zero digit upwards to the next increment of the the non zero number
 
 contract CryptoToken {
-    // uint private tokens = 4000000;
+    // mapping acts like a dictionary to store data
+    // mapping takes a key which maps to a value
+    // mapping(key => value) public mapName
+    mapping(address => uint) public balances;
+    
+   // uint private tokens = 4000000;
 
     uint private totalSupply = 7;
     address public sender;
     
-    address public minter;
+    address private minter;
+
+    // a constructor is a special type of function that gets called immediately upon deployment
+    constructor() {
+        minter = msg.sender;
+    } 
+
 }
