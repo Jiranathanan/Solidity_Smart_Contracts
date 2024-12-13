ef95579cca7972bd8770ef3059ab1668638a4479
Jiranathanan <jiranathanan@gmail.com>
2024-12-13 23:58:20 +0700

    create variables-and-types.sol

---
+++ variables-and-types.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0 ;
+
+contract learnVariables {
+    // keyword for interger is 'uint'
+    uint chocolateBar = 10; // In solidity '-' (dash) is not allowed for naming a variable
+    uint storeOwner = 300;
+
+    // keyword for boolean is 'bool'
+    bool lieDetector = true;
+
+    // keyword for string is 'string'
+    string errorMessageText = "Error! There has been a mistake! :("; 
+
+
+}
